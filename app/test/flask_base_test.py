e63270462f83d7ffd7c7d32c74c070df72aa62bc
ZeCiola <jr.ciola.bricio@gmail.com>
2019-11-18 04:01:26 -0300

    Tests CRUD

--- app/test/flask_base_test.py
+++ app/test/flask_base_test.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 from flask_testing import TestCase
 
 from app.main import db
@@ -6,6 +9,17 @@ from manage import app
 
 class FlaskBaseTest(TestCase):
     """ Base Tests """
+
+    def create_transaction(self):
+        return self.client.post(
+            'api/transaction/cash_change',
+            json={
+                "product_name": ''.join(random.choice(string.ascii_lowercase) for i in range(10)),
+                "product_value": 10,
+                "client_cash": 20
+            }
+        )
+        ...
 
     def create_app(self):
         app.config.from_object('app.main.config.TestingConfig')
--- app/test/test_controller/test_transactions_controller.py
+++ app/test/test_controller/test_transactions_controller.py
@@ -10,8 +10,9 @@ class TestTransactionsCashChange(FlaskBaseTest):
         result = self.client.post(
             'api/transaction/cash_change',
             json={
-                "product_value": 110,
-                "client_cash": 200
+                "product_name": 'Test Notebook',
+                "product_value": 111.11,
+                "client_cash": 222.22
             }
         )
 
@@ -20,16 +21,26 @@ class TestTransactionsCashChange(FlaskBaseTest):
         expect = {
             "status": "success",
             "msg": "Transaction as confirmed",
+            "product_name": "Test Notebook",
             "cash_change_calculator": {
-                "product_value": 110,
-                "client_cash": 200,
-                "cash_change": 90,
+                "product_value": 111.11,
+                "client_cash": 222.22,
+                "cash_change": 111.11,
                 "money_bills": [
                     {
-                        "money_50.0": 1
+                        "money_100.0": 1
                     },
                     {
-                        "money_20.0": 2
+                        "money_10.0": 1
+                    },
+                    {
+                        "money_1.0": 1
+                    },
+                    {
+                        "money_0.1": 1
+                    },
+                    {
+                        "money_0.01": 1
                     }
                 ]
             }
@@ -51,6 +62,7 @@ class TestTransactionsCashChange(FlaskBaseTest):
         expect = {
             "status": "success",
             "msg": "Transaction as confirmed",
+            "product_name": "dasadasd dWW",
             "cash_change_calculator": {
                 "product_value": 5458.54,
                 "client_cash": 7985.12,
@@ -134,6 +146,64 @@ class TestTransactionsCashChange(FlaskBaseTest):
                     'product_value': "'product_value' is a required property"
                 }, 'message': 'Input payload validation failed'
         }
+
+        self.assertDictEqual(result.json, expect)
+
+    def test_transactions_list_all(self):
+        self.create_transaction()
+        self.create_transaction()
+        self.create_transaction()
+
+        result = self.client.get(
+            'api/transaction/list',
+        )
+
+        self.assertStatus(result, HTTPStatus.OK)
+
+        self.assertEqual(len(result.json), 3)
+
+    def test_transactions_update(self):
+        self.create_transaction()
+
+        payload = {
+            "new_product_name": "Teste",
+            "transaction_id": 1
+        }
+
+        result = self.client.put(
+            '/api/transaction/product_name/update',
+            json=payload
+        )
+
+        self.assertStatus(result, HTTPStatus.OK)
+
+        expect = dict(
+            status='success',
+            msg=f'Transaction {payload["transaction_id"]} was Updated',
+            new_name=payload['new_product_name']
+        )
+
+        self.assertDictEqual(result.json, expect)
+        ...
+
+    def test_transactions_delete(self):
+        self.create_transaction()
+
+        payload = {
+            "transaction_id": 1
+        }
+
+        result = self.client.delete(
+            '/api/transaction/delete',
+            json=payload
+        )
+
+        self.assertStatus(result, HTTPStatus.OK)
+
+        expect = dict(
+            status='success',
+            msg=f'Transaction {payload["transaction_id"]} was Deleted'
+        )
 
         self.assertDictEqual(result.json, expect)
 
